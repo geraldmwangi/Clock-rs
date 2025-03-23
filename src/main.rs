7caad065a426926dd17ad6721822d2f06714ed66
Gerald Mwangi <Geraldmwangi@gmx.de>
2025-03-23 09:37:25 +0100

    basic embassy main compiles

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,14 @@
 #![no_main]
 
 use {defmt_rtt as _, embassy_executor::Spawner, embassy_rp::gpio::{Level, Output}, panic_probe as _}; // global logger
-
+use defmt::{error, warn, info, debug, trace};
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
+    // defmt::timestamp!("Starting MyProgram...");
     // let p = embassy_nrf::init(Default::default());
-    let p=embassy_rp::init(Default::default());
+    // let p=embassy_rp::init(Default::default());
 
-    let led = Output::new(p.PIN_13, Level::Low);
+    // let led = Output::new(p.PIN_13, Level::Low);
     // unwrap!(spawner.spawn(blinker(led, Duration::from_millis(300))));
     // spawner.spawn(blinker(led, Duration::from_millis(300))).unwrap();
 }
