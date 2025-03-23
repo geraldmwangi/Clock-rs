0553d0b768b8c7a0c67b7f747a5fc39a78a5ac13
Gerald Mwangi <Geraldmwangi@gmx.de>
2025-03-23 09:59:31 +0100

    include i2c

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,20 @@
 
 use {defmt_rtt as _, embassy_executor::Spawner, embassy_rp::gpio::{Level, Output}, panic_probe as _}; // global logger
 use defmt::{error, warn, info, debug, trace};
+use embassy_rp::{bind_interrupts, i2c::{self, Config, InterruptHandler}, peripherals::I2C1};
+
+bind_interrupts!(struct Irqs {
+    I2C1_IRQ => InterruptHandler<I2C1>;
+});
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
-    // defmt::timestamp!("Starting MyProgram...");
-    // let p = embassy_nrf::init(Default::default());
-    // let p=embassy_rp::init(Default::default());
+    
+    
+    let p=embassy_rp::init(Default::default());
+    let sda=p.PIN_6;
+    let scl=p.PIN_7;
+    info!("set up i2c ");
+    let mut i2c = i2c::I2c::new_async(p.I2C1, scl, sda, Irqs, Config::default());
 
     // let led = Output::new(p.PIN_13, Level::Low);
     // unwrap!(spawner.spawn(blinker(led, Duration::from_millis(300))));
